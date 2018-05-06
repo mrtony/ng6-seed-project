5fa51d74b4ec8e7a4cb6032e1f92b8081e411675
TonyChen <tonychenn@gmail.com>
2018-05-06 19:22:02 +0800

    ch6-7_container pass data to child component using @Input and property

--- src/app/passenger-dashboard/components/product-count/product-count.component.ts
+++ src/app/passenger-dashboard/components/product-count/product-count.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Passenger } from './../../../models/passenger.interface';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-count',
   template: `
-    <p>
-      product-count works!
-    </p>
+    <div>
+      <h3 class="font-bold">Airline Passengers</h3>
+      <h3>
+        Total Passengers: {{ items?.length}}
+      </h3>
+      <h4 class="text-green">
+        Total Checked-In: {{ checkedInCount() }} / {{ items?.length}}
+      </h4>
+    </div>
   `,
   styles: []
 })
 export class ProductCountComponent implements OnInit {
+  @Input() items: Passenger[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkedInCount(): number {
+    if (this.items) {
+      return this.items.filter((p) => p.checkedIn).length;
+    }
   }
 
 }
