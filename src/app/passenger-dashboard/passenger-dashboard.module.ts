316efd11b2500e0ca5feda875a9d77bd7551ac52
TonyChen <tonychenn@gmail.com>
2018-05-07 21:30:04 +0800

    ch7-3-1_升級http為httpClient

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 import { PassengerDashboardService } from './passenger-dashboard.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpClientModule
   ],
   declarations: [
     PassengerDashboardComponent,
--- src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,9 +1,10 @@
+import { Response } from '@angular/http';
 import { environment } from './../../environments/environment';
 import { Passenger } from './../models/passenger.interface';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map, filter, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 const API_HOST = environment.production ? '' : 'http://localhost:3000';
 const PASSENGER_API = `${API_HOST}/api/passengers`;
@@ -13,13 +14,10 @@ const PASSENGER_API = `${API_HOST}/api/passengers`;
 })
 export class PassengerDashboardService {
 
-  constructor(private http: Http) { }
+  constructor(private httpClient: HttpClient) { }
 
   getPassengers(): Observable<Passenger[]> {
-    return this.http
-      .get(PASSENGER_API)
-      .pipe(
-        map<Response, Passenger[]>((response: Response) => response.json())
-      );
+    return this.httpClient
+      .get<Passenger[]>(PASSENGER_API);
   }
 }
