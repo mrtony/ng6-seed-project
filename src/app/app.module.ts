881d7076624a393e05c2fa71088661de156bde4b
TonyChen <tonychenn@gmail.com>
2018-05-10 23:41:34 +0800

    ch9-2_Root module routes and outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Ch5RenderFlowComponent } from './ch5-render-flow/ch5-render-flow.component';
+import { HomeComponent } from './home.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ch5RenderFlowComponent
+    Ch5RenderFlowComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     // Customer Modules
-    PassengerDashboardModule
+    PassengerDashboardModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h3>Airline Passenger app!</h3>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
